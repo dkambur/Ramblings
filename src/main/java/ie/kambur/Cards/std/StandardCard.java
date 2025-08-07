fab5eaf414c7e1f0fdd0d26b95e9c8c66734180f
Dalen <dalen.kambur@gmail.com>
2025-08-07 18:26:52 +0100

    Corrected few old things forgotten about

--- src/main/java/ie/kambur/Cards/std/StandardCard.java
+++ src/main/java/ie/kambur/Cards/std/StandardCard.java
@@ -2,10 +2,10 @@ package ie.kambur.Cards.std;
 
 import ie.kambur.Cards.interfaces.Card;
 
-class StandardCard implements Card {
+public class StandardCard implements Card {
 	
 	private enum colour { SPADES (0), CLUBS (1), DIAMONDS (2), HEARTS (3);
-		private int colour;
+		final private int colour;
 		
 		colour (int code) {
 			colour = code;
@@ -14,10 +14,10 @@ class StandardCard implements Card {
 		int getColourCode() {
 			return colour;
 		}
-	};
+	}
 	
 	private enum position { ACE(0), TWO(1), THREE(2), FOUR(3), FIVE(4), SIX(5), SEVEN(6), EIGHT(7), NINE(8), TEN(9), JACK(10), QUEEN(11), KING(12);
-		private int position;
+		final private int position;
 		
 		position (int code) {
 			position = code;
@@ -26,13 +26,12 @@ class StandardCard implements Card {
 		int getPosition() {
 			return position;
 		}
-	};
+	}
 	
-	private colour theColour;
-	private position thePosition;
+	final private colour theColour;
+	final private position thePosition;
 	
 	public StandardCard (int ordinal) {
-		
 		this.theColour = colour.values()[ordinal/13];
 		this.thePosition = position.values()[ordinal%13];
 	}
--- src/test/java/ie/kambur/Cards/Tester.java
+++ src/test/java/ie/kambur/Cards/Tester.java
@@ -1,8 +1,7 @@
 package ie.kambur.Cards;
 
 
-import java.util.Iterator;
-import ie.kambur.Cards.interfaces.Card;
+import ie.kambur.Cards.std.StandardCard;
 import ie.kambur.Cards.std.StandardDeck;
 
 import org.apache.logging.log4j.LogManager;
@@ -12,20 +11,18 @@ public class Tester {
 	protected static final Logger logger = LogManager.getLogger(Tester.class);
 
 	public static void main(String[] params) {
-        var abc = new ShuffledDeck (new StandardDeck());
+        var abc = new ShuffledDeck<> (new StandardDeck());
 		
 		int i = 0;
-		
-		for (Iterator<Card> cardIt = abc.iterator(); cardIt.hasNext(); ) {
-			Card c = cardIt.next();
 
+        for (StandardCard c : abc) {
             logger.info("Got {}:{}", c, c.returnOrdinalPosition());
-			
-			if (++i % 2 == 0) {
-				abc.returnCard (c);
 
-				logger.info ("Returned {}", c);
-			}
-		}
+            if (++i % 2 == 0) {
+                abc.returnCard(c);
+
+                logger.info("Returned {}", c);
+            }
+        }
 	}
 }
