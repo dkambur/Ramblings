68a759744cfb591fab2e481b81086f1e7ee504f0
Dalen <dalen.kambur@gmail.com>
2025-11-21 20:50:19 +0000

    fixed logging

--- src/main/java/ie/kambur/Cards/service/MessageIdMDCFilter.java
+++ src/main/java/ie/kambur/Cards/service/MessageIdMDCFilter.java
@@ -6,13 +6,17 @@ import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 @Provider
 public class MessageIdMDCFilter implements ContainerRequestFilter, ContainerResponseFilter {
+    private static final Logger logger = LoggerFactory.getLogger(MessageIdMDCFilter.class);
 
     @Override
     public void filter(ContainerRequestContext ctx) {
         MDC.put("messageId", ctx.getHeaderString("Message-Id"));
+        logger.info ("Received request {} {}", ctx.getMethod(), ctx.getUriInfo().getPath());
     }
 
     @Override
